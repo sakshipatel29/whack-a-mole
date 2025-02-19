b5e0c85116332fd31c89377fe002246eb15f296f
sakshipatel29 <sakshirpatel29062002@gmail.com>
2025-02-18 22:42:08 -0600

    mole and plant added

--- mole.js
+++ mole.js
@@ -1,4 +1,5 @@
 let currMoleTile;
+let currPlantTile;
 
 
 window.onload = function() {
@@ -11,13 +12,43 @@ function setGame() {
         tile.id = i.toString();
         document.getElementById("board").appendChild(tile);
     }
+    setInterval(setMole, 1000);
+    setInterval(setPlant, 2000);
 }
 
+function getRandomTile(){
+    let num = Math.floor(Math.random() * 9);
+    return num.toString();
 
+}
 
 function setMole(){
+    if(currMoleTile){
+        currMoleTile.innerHTML = "";
+    }
     let mole = document.createElement("img");
     mole.src = "./images/monty-mole.png";
 
     let num  = getRandomTile();
+    if( currPlantTile && currPlantTile.id == num){
+        return;
+    }
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
+}
+
+function setPlant(){
+    if(currPlantTile){
+        currPlantTile.innerHTML = "";
+    }
+
+    let plant = document.createElement("img");
+    plant.src = "./images/piranha-plant.png";
+
+    let num  = getRandomTile();
+    if( currMoleTile && currMoleTile.id == num){
+        return;
+    }
+    currPlantTile = document.getElementById(num);
+    currPlantTile.appendChild(plant);
 }
